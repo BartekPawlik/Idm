87e10fd90ef924f820ec3348185338301d82c643
Bartlomiej Pawlik <pawlikbartlomiej98@gmail.com>
2025-05-13 15:16:28 +0200

    items section

--- main.js
+++ main.js
@@ -1,44 +1,162 @@
-document.addEventListener("DOMContentLoaded", () => {
-  loadFragment("/01._homepage/01._navbar/nav.html", "nav", initNavMenu);
-  loadFragment("/01._homepage/02._title/title.html", "title");
-  loadFragment("/01._homepage/03._item_carousel/item_carousel.html", "item-carousel", initCarousle);
+const menuBtn = document.querySelectorAll(".menu-btn");
+const navContainer = document.querySelector(".nav-container");
+const overlay = document.querySelector("#overlay");
+const navigation = document.querySelectorAll(".navigation");
+const popup = document.querySelector(".popup");
+
+menuBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+  });
 });
 
-//  Load Section
-function loadFragment(file, elementId, callback) {
-  fetch(file)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(elementId).innerHTML = data;
+overlay.addEventListener("click", () => {
+  navContainer.classList.remove("active");
+});
 
-      if (typeof callback === "function") {
-        callback();
+navigation.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    navContainer.classList.remove("active");
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 24,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    925: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+
+    1439: {
+      slidesPerView: 3,
+    },
+
+    1896: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+// API
+
+const control = document.querySelector("#control");
+const itemContainer = document.querySelector("#items-contener");
+
+const select = document.createElement("select");
+select.id = "quantity";
+select.size = 1;
+
+for (let i = 1; i <= 36; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.textContent = i;
+  if (i === 14) option.selected = true;
+  select.appendChild(option);
+}
+
+control.appendChild(select);
+
+let pageNumber = 1;
+let pageSize = parseInt(select.value);
+let isLoading = false;
+
+select.addEventListener("change", () => {
+  pageSize = parseInt(select.value);
+  pageNumber = 1;
+  itemContainer.innerHTML = "";
+  loadItems();
+});
+
+async function loadItems() {
+  if (isLoading) return;
+  isLoading = true;
+
+
+  try {
+    const res = await fetch(
+      `https://brandstestowy.smallhost.pl/api/random?pageNumber=${pageNumber}&pageSize=${pageSize}`
+    );
+    const items = await res.json();
+
+    const currentItemCount = itemContainer.children.length;
+
+    items.data.forEach((item, index) => {
+      const div = document.createElement("div");
+      div.classList.add("item-container");
+      console.log(pageNumber);
+      // tylko raz przy pierwszym ładowaniu
+
+      div.style.order = currentItemCount + index + 1;
+
+      const img = document.createElement("img");
+      img.src = item.image;
+      img.alt = item.alt;
+
+      const text = document.createElement("span");
+      text.textContent = `ID: ${String(item.id).padStart(2, 0)}`;
+      div.appendChild(text);
+      div.appendChild(img);
+
+      div.addEventListener("click", () => {
+        popup.innerHTML = `
+      <div class="popup-content">
+        <div class="close-container">
+          <p>ID: ${String(item.id).padStart(2, 0)}</p>
+          <button class="popup-exit">✖ CLOSE</button>
+        </div>
+        <img src="${item.image}" alt="${item.alt}">
+      </div>
+    `;
+
+        popup.classList.add("active");
+      });
+
+      itemContainer.appendChild(div);
+    });
+
+    popup.addEventListener("click", (e) => {
+      if (e.target.classList.contains("popup-exit") || e.target === popup) {
+        popup.classList.remove("active");
+        popup.innerHTML = "";
       }
     });
+
+    pageNumber++;
+  } catch (err) {
+    console.error("problem load", err);
+  } finally {
+    isLoading = false;
+
+  }
 }
 
+let scrollTimeout;
+window.addEventListener("scroll", () => {
+  if (scrollTimeout) clearTimeout(scrollTimeout);
 
-function initNavMenu() {
-  const menuBtn = document.querySelectorAll(".menu-btn");
-  const navContainer = document.querySelector(".nav-container");
+  scrollTimeout = setTimeout(() => {
+    const nearBottom =
+      Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight;
 
-
-  menuBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      navContainer.classList.toggle("active");
-    });
-  });
-}
-
-function initCarousle(){
-
-const swiper = new Swiper('.swiper', {
-  // Add any required Swiper options here
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  slidesPerView: 4, // Adjust as needed
-  spaceBetween: 10, // Adjust as needed
+    if (nearBottom) {
+      loadItems();
+    }
+  }, 150);
 });
-}
+
+loadItems();
